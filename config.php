5eb4ad7e025dc2e2b06713dd98211d065bad957a
Miroslav Ivanov <miroslav.ivanov@gmail.com>
2026-04-27 13:23:15 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+// ---------------------------------------------------------
+// ------------- HOST DEPENDANT CONFIGURATION --------------
+// ---------------------------------------------------------
+
+/**
+ * Database configuration
+ */
+const DB_SERVER = 'mysql';
+const DB_HOST = '';
+const DB_USER = 'root';
+const DB_PASS = 'root';
+
+/**
+ * Application
+ */
+const OS_URL = '';
